02ea7f09a3967e4a37cdb34b125371608fed3849
gabrielgiroe <gabriel.giroe@gmail.com>
2021-07-15 10:53:43 +0300

    Resolve titleize problem

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,25 +1,34 @@
-  def echo (gstrin)
-     gstrin
-  end
+def echo (gstrin)
+  gstrin
+end
 
-  def shout gstring
-    gstring.strip()
-    gstring=gstring.upcase()
-  end
+def shout gstring
+  gstring.strip()
+  gstring=gstring.upcase()
+end
 
-  def   repeat (gstring, noumbers=2)
-    return ([gstring]* noumbers).join(" ")
-  end
+def   repeat (gstring, noumbers=2)
+  return ([gstring]* noumbers).join(" ")
+end
 
-  def start_of_word (gstring,noumber=0)
-    return gstring[0,noumber]
-  end
+def start_of_word (gstring,noumber=0)
+  return gstring[0,noumber]
+end
 
-  def first_word(gstring)
-    return  gstring.split.first()
-  end
+def first_word(gstring)
+  return  gstring.split.first()
+end
 
-  def titleize(string)
-    string.split.map { |word| word.capitalize }.join
+def titleize(string)
+  lower_words = ["or", "and", "over", "the"]
+  if string.strip.include? " "
+  resolve_string=string.split.map {  |word| lower_words.include?(word)? word  : word.capitalize  }.join(" ")
+   else
+    resolve_string=string.split.map { |word| lower_words.include?(word)? word  : word.capitalize }.join(" ")
   end
+  if resolve_string[0]==="t"
+  resolve_string[0]="T"
+  end
+  resolve_string
+end
 
