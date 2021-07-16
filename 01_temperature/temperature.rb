b1bd0ada01bd6b96fbfcf068bd9109f51255e645
gabrielgiroe <gabriel.giroe@gmail.com>
2021-07-16 10:11:30 +0300

    Refactor temperature solutions

--- 01_temperature/temperature.rb
+++ 01_temperature/temperature.rb
@@ -1,14 +1,7 @@
 def ftoc(param)
-  if param ===32
-    0
-    else
-       5.0*(param-32)/9
-  end
+  5.0 * (param - 32) / 9
 end
 
 def ctof (param)
-  if param===0
-    32
-    else 9.0*param/5.0+32
-  end
+  9.0 * param / 5.0 + 32
 end
