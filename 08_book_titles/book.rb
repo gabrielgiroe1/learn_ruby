ab760d6bb8d954c0195c2ea2662e0463897fe27d
gabrielgiroe <gabriel.giroe@gmail.com>
2021-07-19 14:53:29 +0300

    Freeze the array

--- 08_book_titles/book.rb
+++ 08_book_titles/book.rb
@@ -2,7 +2,7 @@ class Book
   attr_writer :title
 
   def title
-    lower_words = %w[or and over the a an in of]
+    lower_words = %w[or and over the a an in of].freeze
     @title.capitalize.split.map { |word| lower_words.include?(word) ? word : word.capitalize }.join(" ")
   end
 end
