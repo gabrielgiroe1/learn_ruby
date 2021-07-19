22ade596c969452fcf0f1acb4bdede7d53208e61
gabrielgiroe <gabriel.giroe@gmail.com>
2021-07-19 09:40:11 +0300

    Resolve book_titles

---
+++ 08_book_titles/book.rb
@@ -0,0 +1,8 @@
+class Book
+  attr_writer :title
+
+  def title
+    lower_words = %w[or and over the a an in of]
+    @title.capitalize.split.map { |word| lower_words.include?(word) ? word : word.capitalize }.join(" ")
+  end
+end
