fce484cb4896a2696f2a1a2073ba7e7f3fa13722
gabrielgiroe <gabriel.giroe@gmail.com>
2021-07-20 10:13:27 +0300

    Resolve timer problem

--- 09_timer/timer.rb
+++ 09_timer/timer.rb
@@ -11,15 +11,6 @@ class Timer
   end
 
   def time_string
-    h = @seconds.div(3600)
-    if h > 0
-      @seconds -= h * 3600
-    end
-    m = @seconds.div(60)
-    if m > 0
-      @seconds -= m * 60
-    end
-    s = @seconds
-    "#{ padded(h)}:#{padded(m)}:#{padded(s) }"
+    Time.at(@seconds).utc.strftime("%H:%M:%S")
   end
 end
